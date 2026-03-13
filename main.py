081ecddd42b1f83dc94bcbb2e5171226442a6161
873pp-collab <patelparth873.pp@gmail.com>
2026-03-13 16:08:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from flask import Flask, request
 import os
 from delta_rest_client import DeltaRestClient, OrderType
+
+app = Flask(__name__)
 
 API_KEY = os.environ.get("DELTA_API_KEY")
 API_SECRET = os.environ.get("DELTA_API_SECRET")
 
 delta_client = DeltaRestClient(
-    base_url='https://demo-api.india.delta.exchange',
+    base_url="https://demo-api.india.delta.exchange",
     api_key=API_KEY,
     api_secret=API_SECRET
 )
@@ -19,19 +22,16 @@ current_position = None
 def buy():
     global current_position
 
-    if current_position == "LONG":
-        print("Already in BUY")
-        return
+    print("Executing BUY order")
 
-    if current_position == "SHORT":
-        close_position()
-
-    delta_client.place_order(
+    response = delta_client.place_order(
         product_id=PRODUCT_ID,
         size=ORDER_SIZE,
-        side='buy',
+        side="buy",
         order_type=OrderType.MARKET
     )
+
+    print("Delta response:", response)
 
     current_position = "LONG"
 
@@ -39,46 +39,40 @@ def buy():
 def sell():
     global current_position
 
-    if current_position == "SHORT":
-        print("Already in SELL")
-        return
+    print("Executing SELL order")
 
-    if current_position == "LONG":
-        close_position()
-
-    delta_client.place_order(
+    response = delta_client.place_order(
         product_id=PRODUCT_ID,
         size=ORDER_SIZE,
-        side='sell',
+        side="sell",
         order_type=OrderType.MARKET
     )
+
+    print("Delta response:", response)
 
     current_position = "SHORT"
 
 
-def close_position():
-    global current_position
-
-    if current_position is None:
-        return
-
-    side = 'sell' if current_position == "LONG" else 'buy'
-
-    delta_client.place_order(
-        product_id=PRODUCT_ID,
-        size=ORDER_SIZE,
-        side=side,
-        order_type=OrderType.MARKET
-    )
-
-    current_position = None
-
-
 def handle_signal(signal):
+
     signal = signal.upper()
+
+    print("Signal received:", signal)
 
     if "BUY" in signal:
         buy()
 
     elif "SELL" in signal:
         sell()
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+
+    data = request.data.decode("utf-8")
+
+    print("Webhook received:", data)
+
+    handle_signal(data)
+
+    return "ok", 200
